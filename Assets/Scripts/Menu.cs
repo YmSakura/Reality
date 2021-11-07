2fd1c4cf32ac36a42dabb662f28434f062b0c7bb
Ym <1098548238@qq.com>
2021-11-07 10:30:59 +0800

    <feat>(Scripts):MainMenu&PauseMenu
    增加了主菜单和暂停菜单的UI

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    private void UIEnable()
+    {
+        //新学的函数Find，通过Hierachy窗口的目录来查找物体
+        GameObject.Find("Canvas/Menu/UI").SetActive(true);
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+
+}
